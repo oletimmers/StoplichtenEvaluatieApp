6058e734934f6a286e68e7823be723a8ea06a066
Ole Timmers <s1115222@studen.hsleiden.nl>
2020-06-19 22:48:52 +0200

    19-6-2020  Material design toegevoegd  Knoppen veranderd  HomescreenActivity even wat UI gegeven

--- app/src/main/java/com/example/stoplichtenevaluatieapp/HomeScreen.java
+++ app/src/main/java/com/example/stoplichtenevaluatieapp/HomeScreen.java
@@ -23,7 +23,7 @@ public class HomeScreen extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Nieuwe bijeenkomst toevoegen", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
