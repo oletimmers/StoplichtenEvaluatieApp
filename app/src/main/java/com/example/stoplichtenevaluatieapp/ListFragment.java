9c495803518ab96b67d59fb132fd98691782767f
Ole Timmers <s1115222@studen.hsleiden.nl>
2020-06-22 17:12:35 +0200

    Wisselen van dagen werkt nu top

--- app/src/main/java/com/example/stoplichtenevaluatieapp/ListFragment.java
+++ app/src/main/java/com/example/stoplichtenevaluatieapp/ListFragment.java
@@ -5,40 +5,40 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.stoplichtenevaluatieapp.adapters.MeetingAdapter;
 import com.example.stoplichtenevaluatieapp.models.Meeting;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.Timestamp;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.reflect.Array;
-import java.time.LocalDateTime;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.Set;
 
 public class ListFragment extends Fragment {
     private static final String TAG = "ListFragment";
+
+    TextView dateToShow;
 
     ArrayList<Meeting> meetings;
     ListView meetingsView;
     private static MeetingAdapter adapter;
     public static Date dt = new Date();
+
+    RelativeLayout loadingPanel;
 
     @Override
     public View onCreateView(
@@ -53,21 +53,56 @@ public class ListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        dateToShow = view.findViewById(R.id.dateToShow);
+        updateTime();
+
         meetingsView = (ListView)view.findViewById(R.id.meetingsThisDay);
         meetings = new ArrayList<Meeting>();
+
+        loadingPanel = view.findViewById(R.id.loadingPanel);
+
+        ImageView left = view.findViewById(R.id.button_left);
+        ImageView right = view.findViewById(R.id.button_right);
+
+        left.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(dt);
+                calendar.add(Calendar.DATE, -1);
+                dt = calendar.getTime();
+                updateTime();
+
+                meetings = new ArrayList<Meeting>();
+                getMeetings();
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(dt);
+                calendar.add(Calendar.DATE, 1);
+                dt = calendar.getTime();
+                updateTime();
+
+                meetings = new ArrayList<Meeting>();
+                getMeetings();
+            }
+        });
+
         this.getMeetings();
     }
 
     public void getMeetings() {
+        loadingPanel.setVisibility(View.VISIBLE);
+
         // Gisteren en morgen bepalen
-        Snackbar.make(this.getView(), "Get meetings", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
         Calendar yesterday = Calendar.getInstance();
         Calendar tomorrow = Calendar.getInstance();
         yesterday.setTime(dt);
         tomorrow.setTime(dt);
         yesterday.add(Calendar.DATE, -1);
-        tomorrow.add(Calendar.DATE, 1);
+        tomorrow.add(Calendar.DATE, 0);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         db.collection("meetings")
@@ -86,11 +121,16 @@ public class ListFragment extends Fragment {
                             }
                             adapter = new MeetingAdapter(meetings, getContext());
                             meetingsView.setAdapter(adapter);
-
+                            loadingPanel.setVisibility(View.GONE);
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
                 });
+    }
+
+    public void updateTime() {
+        SimpleDateFormat sfd = new SimpleDateFormat("dd-MM-yyyy");
+        dateToShow.setText(sfd.format(dt));
     }
 }
